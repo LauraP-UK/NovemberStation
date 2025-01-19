bb95e3b8df89f527c7824180afdc34cfac81a96f
Laura Price <lauraprice356@gmail.com>
2025-01-19 18:10:19 +0000

    Maks constants

--- Main/Scripts/Items/ItemType.cs
+++ Main/Scripts/Items/ItemType.cs
@@ -1,11 +1,23 @@
-
 using System;
+using System.Collections.Generic;
 using Godot;
 
 public class ItemType {
     private readonly string _itemName, _imagePath, _modelPath, _description;
     private readonly int _itemCost;
     private readonly Action<RigidBody3D> _onSpawn;
+
+    private static readonly List<KeyValuePair<int, bool>>
+        _collisionMasks = new() {
+            new KeyValuePair<int, bool>(1, true),
+            new KeyValuePair<int, bool>(2, true),
+            new KeyValuePair<int, bool>(3, true),
+            new KeyValuePair<int, bool>(4, true)
+        },
+        _collisionLayers = new() {
+            new KeyValuePair<int, bool>(3, true),
+            new KeyValuePair<int, bool>(4, true)
+        };
 
     private ItemType(string itemName, string imagePath, string modelPath, string description, int itemCost, Action<RigidBody3D> onSpawn) {
         _itemName = itemName;
@@ -15,7 +27,7 @@ public class ItemType {
         _itemCost = itemCost;
         _onSpawn = onSpawn;
     }
-    
+
     public string GetItemName() => _itemName;
     public string GetImagePath() => _imagePath;
     public string GetModelPath() => _modelPath;
@@ -23,19 +35,15 @@ public class ItemType {
     public int GetItemCost() => _itemCost;
 
     public Texture2D GetImage() => ResourceLoader.Load<Texture2D>(GetImagePath());
-    
+
     public RigidBody3D CreateInstance() {
         RigidBody3D rigidBody3D = Loader.SafeInstantiate<RigidBody3D>(GetModelPath());
-        rigidBody3D.SetCollisionLayerValue(3, true);
-        rigidBody3D.SetCollisionLayerValue(4, true);
-        rigidBody3D.SetCollisionMaskValue(1, true);
-        rigidBody3D.SetCollisionMaskValue(2, true);
-        rigidBody3D.SetCollisionMaskValue(3, true);
-        rigidBody3D.SetCollisionMaskValue(4, true);
+        _collisionLayers.ForEach(pair => rigidBody3D.SetCollisionLayerValue(pair.Key, pair.Value));
+        _collisionMasks.ForEach(pair => rigidBody3D.SetCollisionMaskValue(pair.Key, pair.Value));
         _onSpawn?.Invoke(rigidBody3D);
         return rigidBody3D;
     }
-    
+
     public ShopItemDisplayButton CreateButton() {
         ShopItemDisplayButton item = new(this);
         item.SetName(GetItemName());
@@ -51,5 +59,4 @@ public class ItemType {
     public static ItemType Create(string itemName, string imagePath, string modelPath, string description, int itemCost, Action<RigidBody3D> onSpawn = null) {
         return new ItemType(itemName, imagePath, modelPath, description, itemCost, onSpawn);
     }
-
 }
