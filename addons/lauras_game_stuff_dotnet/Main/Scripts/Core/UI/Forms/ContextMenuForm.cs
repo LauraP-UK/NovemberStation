f452091c9a12b8d05adbb86f01aaa26f11837fef
Laura Price <lauraprice356@gmail.com>
2025-01-24 22:49:45 +0000

    Context and Actions menu start

---
+++ addons/lauras_game_stuff_dotnet/Main/Scripts/Core/UI/Forms/ContextMenuForm.cs
@@ -0,0 +1,33 @@
+﻿
+using System.Collections.Generic;
+using Godot;
+using NovemberStation.addons.lauras_game_stuff_dotnet.Main.Scripts.Core.UI.FormElements.Containers;
+
+public class ContextMenuForm : FormBase {
+
+    private readonly NinePatchRectElement _mainFrame, _actionsContainerFrame;
+    private readonly ControlElement _actionsContainer;
+    
+    private const string
+        FORM_PATH = "res://Main/Prefabs/UI/GameElements/ContextMenu.tscn",
+        MAIN_FRAME = "Frame",
+        ACTIONS_CONTAINER = "ActionsContainer",
+        ACTIONS_CONTAINER_FRAME = "ActionsContainer/ActionsFrame";
+
+    public ContextMenuForm(string formName) : base(formName, FORM_PATH) {
+        NinePatchRect mainFrame = FindNode<NinePatchRect>(MAIN_FRAME);
+        NinePatchRect actionsContainerFrame = FindNode<NinePatchRect>(ACTIONS_CONTAINER_FRAME);
+        Control actionsContainer = FindNode<Control>(ACTIONS_CONTAINER);
+        
+        _mainFrame = new NinePatchRectElement(mainFrame);
+        _actionsContainerFrame = new NinePatchRectElement(actionsContainerFrame);
+        _actionsContainer = new ControlElement(actionsContainer);
+        
+        _menuElement = new ControlElement(_menu);
+    }
+    
+    
+    protected override List<IFormObject> GetAllElements() => new(){_mainFrame, _actionsContainerFrame, _actionsContainer};
+
+    protected override void OnDestroy() { }
+}
---
+++ addons/lauras_game_stuff_dotnet/Main/Scripts/Core/UI/PremadeMenus/ContextMenu.cs
@@ -0,0 +1,14 @@
+﻿
+using NovemberStation.addons.lauras_game_stuff_dotnet.Main.Scripts.Core.UI;
+
+public class ContextMenu : PreMadeMenu<ContextMenuForm> {
+    protected override FormBase Build() {
+        ContextMenuForm contextMenu = new(GetFormName());
+        
+        
+        return contextMenu;
+    }
+    protected override string GetFormName() {
+        return "ContextMenu";
+    }
+}
