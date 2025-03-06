5e73819d47d202c225e728293c56f2061a9393bb
Laura Price <lauraprice356@gmail.com>
2025-03-06 01:10:02 +0000

    Environment manager and time shifting

--- Main/TestScript.cs
+++ Main/TestScript.cs
@@ -2,120 +2,93 @@ using System.Collections.Generic;
 using Godot;
 
 public partial class TestScript : Node {
-	private readonly Dictionary<RigidBody3D, Vector3> _objSpawns = new();
-	
-	private readonly SmartDictionary<ulong, IObjectBase> _objects = new();
-	private WorldEnvironment _worldEnvironment;
-	private EnvironmentType _currentEnvironment = Environments.MORNING;
-	private static readonly long DAY_LENGTH = 20000L * Environments.GetCount();
-	
-	private long _dayTime = 0L;
+    private readonly Dictionary<RigidBody3D, Vector3> _objSpawns = new();
 
-	public TestScript() {
-		GameManager.Init();
-		GameManager.I().SetActiveScene(this);
-	}
+    private readonly SmartDictionary<ulong, IObjectBase> _objects = new();
 
-	public override void _Input(InputEvent @event) => InputController.ProcessInput(@event);
+    public TestScript() {
+        GameManager.Init();
+        GameManager.I().SetActiveScene(this);
+    }
 
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready() {
-		GD.Print("Start");
-		
-		EventManager.HookWindowResize(GetViewport());
-		UIManager.SetUILayer();
-		
-		GameManager gameManager = GameManager.I();
-		gameManager.SetSceneObjects(GetTree().Root.GetNode<Node3D>("Main/SceneObjects"));
+    public override void _Input(InputEvent @event) => InputController.ProcessInput(@event);
 
-		Input.MouseMode = Input.MouseModeEnum.Captured;
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready() {
+        GD.Print("Start");
 
-		Player player = (Player)Characters.PLAYER.CreateActor();
-		GetTree().Root.GetNode<Node>("Main/PlayerHolder").AddChild(player.GetModel());
-		player.SetPosition(new Vector3(5f, 0.2f, 0f), new Vector3(0.0f, 90.0f, 0.0f));
-		gameManager.SetPlayer(player);
+        EventManager.HookWindowResize(GetViewport());
+        UIManager.SetUILayer();
 
-		Node3D sceneObjects = GetTree().Root.GetNode<Node3D>("Main/SceneObjects");
-		foreach (Node child in sceneObjects.GetChildren()) {
-			if (child is not Node3D obj) continue;
-			gameManager.RegisterObject(obj);
-		}
+        GameManager gameManager = GameManager.I();
+        gameManager.SetSceneObjects(GetTree().Root.GetNode<Node3D>("Main/SceneObjects"));
 
-		foreach (Node child in gameManager.GetSceneObjects().GetChildren()) {
-			if (child is not RigidBody3D obj) continue;
-			_objSpawns.Add(obj, obj.GlobalPosition);
-			obj.AngularDamp = 0.5f;
-		}
+        Input.MouseMode = Input.MouseModeEnum.Captured;
 
-		_worldEnvironment = GetTree().Root.GetNode<WorldEnvironment>("Main/WorldEnvironment");
-		_currentEnvironment.Apply(_worldEnvironment);
+        Player player = (Player)Characters.PLAYER.CreateActor();
+        GetTree().Root.GetNode<Node>("Main/PlayerHolder").AddChild(player.GetModel());
+        player.SetPosition(new Vector3(5f, 0.2f, 0f), new Vector3(0.0f, 90.0f, 0.0f));
+        gameManager.SetPlayer(player);
 
-		GD.Print($"Dynamic Objects: {_objSpawns.Count}");
-		
-		Scheduler.ScheduleRepeating(0L, 1000L, _ => _objects.RemoveWhere(pair => GameUtils.IsNodeInvalid(pair.Value.GetBaseNode3D())));
-	}
+        Node3D sceneObjects = GetTree().Root.GetNode<Node3D>("Main/SceneObjects");
+        foreach (Node child in sceneObjects.GetChildren()) {
+            if (child is not Node3D obj) continue;
+            gameManager.RegisterObject(obj);
+        }
 
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta) {
-		GameManager gameManager = GameManager.I();
-		gameManager.Process(delta);
-		
-		Player player = gameManager.GetPlayer();
-		if (player.GetModel().Position.Y < -20) {
-			player.SetPosition(new Vector3(5f, 0.2f, 0f), new Vector3(0.0f, 90.0f, 0.0f));
-			Toast.Warn(player, "You fell off, you numpty. I'm respawning you...");
-		}
-		
-		DirectionalLight3D sun = GetTree().Root.GetNode<DirectionalLight3D>("Main/SunContainer/Sun");
-		Node3D sunContainer = GetTree().Root.GetNode<Node3D>("Main/SunContainer");
-		Vector3 playerPos = player.GetPosition();
-		sunContainer.SetPosition(new Vector3(playerPos.X, 100, playerPos.Z));
+        foreach (Node child in gameManager.GetSceneObjects().GetChildren()) {
+            if (child is not RigidBody3D obj) continue;
+            _objSpawns.Add(obj, obj.GlobalPosition);
+            obj.AngularDamp = 0.5f;
+        }
 
-		if (!gameManager.GetTree().IsPaused()) {
-			_dayTime += (long)(delta * 1000L);
-			if (_dayTime >= DAY_LENGTH) _dayTime = 0L;
-			
-			long phaseLength = DAY_LENGTH / Environments.GetCount();
-			int phaseIndex = (int)Mathsf.Remap(0L, DAY_LENGTH, _dayTime, 0, Environments.GetCount());
-			float ratio = (_dayTime % phaseLength) / (float)phaseLength;
-			
-			sunContainer.SetRotation(new Vector3(Mathf.DegToRad(Mathsf.Remap(0L, 80000L, _dayTime, 360.0f, 0.0f) + 90.0f),0.0f, 0.0f));
-			
-			EnvironmentType currentEnvironment = _currentEnvironment;
-			_currentEnvironment = Environments.GetAll()[phaseIndex];
-			
-			if (!currentEnvironment.Equals(_currentEnvironment)) Toast.Info(player, $"Time: {_currentEnvironment.GetName()}");
-			EnvironmentType next = currentEnvironment.GetNext();
-			EnvironmentType blend = _currentEnvironment.BlendWith(next, ratio);
-			blend.Apply(_worldEnvironment);
-			
-			GD.Print($"DayTime: {_dayTime}  |  LightEnergy: {sun.LightEnergy}");
-			sun.LightEnergy = _dayTime switch {
-				>= 40000L and <= 50000L => Mathsf.Remap(40000L, 50000L, _dayTime, 1.0f, 0.0f),
-				>= 75000L and <= 80000L => Mathsf.Remap(75000L, 80000L, _dayTime, 0.0f, 1.0f),
-				_ => sun.LightEnergy
-			};
-		}
+        Node3D sunObj = Loader.SafeInstantiate<Node3D>("res://Main/Prefabs/Sandbox/Sun.tscn");
+        AddChild(sunObj);
 
-		Node sceneObjects = gameManager.GetSceneObjects();
-		
-		foreach (Node child in sceneObjects.GetChildren()) {
-			if (child is not RigidBody3D physicsObj) continue;
-			if (!(physicsObj.GlobalPosition.Y < -20)) continue;
-			if (_objSpawns.TryGetValue(physicsObj, out Vector3 origPosition))
-				physicsObj.GlobalPosition = origPosition;
-			else {
-				Toast.Error(player, $"Deleted {gameManager.GetObjectClass(physicsObj.GetInstanceId()).GetType()}");
-				physicsObj.QueueFree();
-			}
-		}
-	}
+        EnvironmentManager.Init(
+            GetTree().Root.GetNode<WorldEnvironment>("Main/WorldEnvironment"),
+            GetTree().Root.GetNode<Node3D>("Main/SunContainer"),
+            sunObj,
+            GetTree().Root.GetNode<DirectionalLight3D>("Main/SunContainer/Sun")
+        );
 
-	public override void _PhysicsProcess(double delta) {
-		base._PhysicsProcess(delta);
-		GameManager.I().PhysicsProcess(delta);
-	}
-	
-	public T GetObjectClass<T>(ulong id) where T : IObjectBase => (T) _objects.GetOrDefault(id, null);
-	public SmartDictionary<ulong, IObjectBase> GetObjects() => _objects;
+        GD.Print($"Dynamic Objects: {_objSpawns.Count}");
+
+        Scheduler.ScheduleRepeating(0L, 1000L, _ => _objects.RemoveWhere(pair => GameUtils.IsNodeInvalid(pair.Value.GetBaseNode3D())));
+    }
+
+    // Called every frame. 'delta' is the elapsed time since the previous frame.
+    public override void _Process(double delta) {
+        GameManager gameManager = GameManager.I();
+        gameManager.Process(delta);
+
+        Player player = gameManager.GetPlayer();
+        if (player.GetModel().Position.Y < -20) {
+            player.SetPosition(new Vector3(5f, 0.2f, 0f), new Vector3(0.0f, 90.0f, 0.0f));
+            Toast.Warn(player, "You fell off, you numpty. I'm respawning you...");
+        }
+
+        if (!gameManager.GetTree().IsPaused()) EnvironmentManager.Process(delta);
+
+        Node sceneObjects = gameManager.GetSceneObjects();
+
+        foreach (Node child in sceneObjects.GetChildren()) {
+            if (child is not RigidBody3D physicsObj) continue;
+            if (!(physicsObj.GlobalPosition.Y < -20)) continue;
+            if (_objSpawns.TryGetValue(physicsObj, out Vector3 origPosition))
+                physicsObj.GlobalPosition = origPosition;
+            else {
+                Toast.Error(player, $"Deleted {gameManager.GetObjectClass(physicsObj.GetInstanceId()).GetType()}");
+                physicsObj.QueueFree();
+            }
+        }
+    }
+
+    public override void _PhysicsProcess(double delta) {
+        base._PhysicsProcess(delta);
+        GameManager.I().PhysicsProcess(delta);
+    }
+
+    public T GetObjectClass<T>(ulong id) where T : IObjectBase => (T)_objects.GetOrDefault(id, null);
+    public SmartDictionary<ulong, IObjectBase> GetObjects() => _objects;
 }
--- addons/lauras_game_stuff_dotnet/Main/Scripts/Core/Environment/EnvironmentManager.cs
+++ addons/lauras_game_stuff_dotnet/Main/Scripts/Core/Environment/EnvironmentManager.cs
@@ -1,4 +1,86 @@
 ﻿
-public class EnvironmentManager {
+using Godot;
+
+public static class EnvironmentManager {
     
+    private static WorldEnvironment _worldEnvironment;
+    private static EnvironmentType _currentEnvironment = Environments.MORNING;
+    private static readonly long DAY_LENGTH = 120000L * Environments.GetCount();
+    private const long SUN_FADE_TIME = 10000L / 2L;
+	
+    private static long _dayTime;
+    
+    private static Node3D _sunContainer, _sunObj;
+    private static DirectionalLight3D _sunLight;
+    private static EnvListeners _envListeners;
+    
+    public static void Init(WorldEnvironment worldEnvironment, Node3D sunContainer, Node3D sunObj, DirectionalLight3D sunLight) {
+        if (_envListeners != null) EventManager.UnregisterListeners(_envListeners);
+        _worldEnvironment = worldEnvironment;
+        _sunContainer = sunContainer;
+        _sunObj = sunObj;
+        _sunLight = sunLight;
+        _envListeners = new EnvListeners();
+        EventManager.RegisterListeners(_envListeners);
+    }
+    
+    public static void Process(double delta) {
+        _dayTime += (long)(delta * 1000L);
+        if (_dayTime >= DAY_LENGTH) _dayTime = 0L;
+
+        Player player = GameManager.I().GetPlayer();
+
+        long phaseLength = DAY_LENGTH / Environments.GetCount();
+        int phaseIndex = (int)Mathsf.Remap(0L, DAY_LENGTH, _dayTime, 0, Environments.GetCount());
+        float ratio = _dayTime % phaseLength / (float)phaseLength;
+
+        float sunRotationDegs = Mathsf.Remap(0L, DAY_LENGTH, _dayTime, 360.0f, 0.0f) + 90.0f; // 90 degrees offset because we start at morning
+        Vector3 sunRot = new(Mathf.DegToRad(sunRotationDegs), 0.0f, 0.0f);
+        _sunContainer?.SetRotation(sunRot);
+        _sunObj?.SetRotation(sunRot);
+        _sunObj?.SetPosition(player.GetPosition());
+
+        EnvironmentType currentEnvironment = _currentEnvironment;
+        _currentEnvironment = Environments.GetAll()[phaseIndex];
+			
+        if (!currentEnvironment.Equals(_currentEnvironment)) Toast.Info(player, $"Time: {_currentEnvironment.GetName()}");
+        EnvironmentType next = currentEnvironment.GetNext();
+        EnvironmentType blend = _currentEnvironment.BlendWith(next, ratio);
+        if (_worldEnvironment != null) blend.Apply(_worldEnvironment);
+
+        if (_sunLight == null) return;
+        if (_dayTime >= (DAY_LENGTH / 2L) - SUN_FADE_TIME && _dayTime <= (DAY_LENGTH / 2L) + SUN_FADE_TIME) {
+            _sunLight.LightEnergy = Mathsf.Remap(DAY_LENGTH / 2L - SUN_FADE_TIME, DAY_LENGTH / 2L + SUN_FADE_TIME, _dayTime, 1.0f, 0.0f);
+        } else if (_dayTime >= (DAY_LENGTH / 2L) + SUN_FADE_TIME && _dayTime <= DAY_LENGTH - SUN_FADE_TIME) {
+            _sunLight.LightEnergy = 0.0f;
+        } else if (_dayTime >= DAY_LENGTH - SUN_FADE_TIME || _dayTime <= SUN_FADE_TIME) {
+            
+            float startTime = DAY_LENGTH - SUN_FADE_TIME;
+            float endTime = SUN_FADE_TIME + DAY_LENGTH;
+            float adjustedDayTime = _dayTime < SUN_FADE_TIME ? _dayTime + DAY_LENGTH : _dayTime;
+
+            _sunLight.LightEnergy = Mathsf.Remap(startTime, endTime, adjustedDayTime, 0.0f, 1.0f);
+        } else {
+            _sunLight.LightEnergy = 1.0f;
+        }
+    }
+
+    private static void AddTime(long time) {
+        Toast.Info(GameManager.I().GetPlayer(), $"{(time >= 0 ? "Adding" : "Subtracting")} {Mathsf.Round(time / 1000L, 2)} seconds");
+        _dayTime = (long)Mathf.Wrap(_dayTime + time, 0L, DAY_LENGTH);
+    }
+
+    private class EnvListeners {
+        private const long DAY_ADD = 15000L;
+        
+        [EventListener]
+        public void OnKeyPressed(KeyPressEvent ev, Key key) {
+            if (ev.IsCaptured()) return;
+            if (key == Key.R) {
+                AddTime(DAY_ADD);
+            } else if (key == Key.T) {
+                AddTime(-DAY_ADD);
+            }
+        }
+    }
 }
